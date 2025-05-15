eef8d50a68ff86f953d2600aaefab218d5ab310f
10Kimi <wiseoutput@gmail.com>
2025-05-15 14:58:17 +0900

    Update flask_kw_evaluator_api.py

--- flask_kw_evaluator_api.py
+++ flask_kw_evaluator_api.py
@@ -107,4 +107,6 @@ def evaluate_keywords():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+      import os
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
